0acc0c022813549429766d2b4b775b860d3205d8
Sarter Wagner <hobitt.as@gmail.com>
2020-10-15 18:59:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   n = Math.abs(n);
+   let res = 0, check = 1, ban = n;
+   while (ban >= 10) {
+      ban = Math.floor(ban / 10);
+      check *= 10;
+   }
+   while (n > 0.99) {
+      res += (n % 10) * check;
+      check /= 10;
+      n = Math.floor(n / 10);
+   }
+
+   return Math.abs(res);
 }
